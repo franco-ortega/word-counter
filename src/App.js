7d0d8244f8b47db2c7b8801ab10f3a3166bec9e0
Franco Ortega <francopdx@gmail.com>
2022-08-22 16:57:41 -0700

    count words with regex filter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { countWords } from './utils/countWords';
+import styles from './App.module.css';
 
-function App() {
+const App = () => {
+  const [story, setStory] = useState('');
+  const [wordCount, setWordCount] = useState(null);
+  const [sortedWords, setSortedWords] = useState([]);
+
+  const onStorySubmit = (e) => {
+    e.preventDefault();
+
+    const cleanString = story
+      .replace(/\n/g, ' ')
+      .replace(/([^A-Za-z0-9 ])/g, '');
+    console.log(cleanString);
+
+    const splitStory = cleanString.split(' ');
+
+    const wordCount = countWords(splitStory);
+
+    setWordCount(wordCount);
+
+    const words = Object.keys(wordCount);
+
+    const sortedWords = words.sort((a, b) => {
+      return wordCount[b] - wordCount[a];
+    });
+    setSortedWords(sortedWords);
+
+    console.log(sortedWords);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <h1>Word Counter</h1>
+      <form onSubmit={onStorySubmit}>
+        <label htmlFor='story'>
+          <textarea id='story' onChange={(e) => setStory(e.target.value)} />
+        </label>
+        <button>Submit</button>
+      </form>
+      <ul>
+        {sortedWords.map((word, i) => (
+          <li key={i}>
+            <p>
+              {i + 1 < 10 ? `0${i + 1}` : `${i + 1}`}. {word}:
+            </p>
+
+            <p>{wordCount[word]}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
